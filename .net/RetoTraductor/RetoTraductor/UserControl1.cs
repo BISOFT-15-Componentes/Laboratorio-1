e57d73c669bdcc5a0935abc637a01efc86070d8d
ajsalasa <abraham.salas.alfaro@intel.com>
2023-02-01 23:59:39 -0600

    Add translator

---
+++ .net/RetoTraductor/RetoTraductor/UserControl1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RetoTraductor
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> englishToSpanishDictionary = new Dictionary<string, string>
+            {
+                {"hello", "hola"},
+                {"yes", "sí"},
+                {"no", "no"},
+                {"dog", "perro"},
+                {"cat", "gato"},
+                {"house", "casa"},
+                {"car", "coche"},
+                {"book", "libro"},
+                {"computer", "computadora"},
+                {"glass", "vaso"}
+            };
+
+            string englishWord = textBox1.Text;
+
+            if (englishToSpanishDictionary.ContainsKey(englishWord.ToLower()))
+            {
+                label1.Text = englishToSpanishDictionary[englishWord.ToLower()];
+            }
+            else
+            {
+                label1.Text = "The word is not found in the dictionary.";
+            }
+        }
+    }
+}
